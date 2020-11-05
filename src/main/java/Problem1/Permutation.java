e82b5821d58c73190cce5c350d82ad65c28e4c75
zhamid01 <zhamid01@uw.edu>
2020-11-05 00:44:30 -0800

    Permutation work

--- src/main/java/Problem1/Permutation.java
+++ src/main/java/Problem1/Permutation.java
@@ -5,7 +5,30 @@ import java.util.List;
 
 public class Permutation {
     public static List<List<Integer>> permute(int[] nums) {
-        // homework
-        return new ArrayList<>(); // place holder
+        List<List<Integer>> n1 = new ArrayList<List<Integer>>();
+        permuteHelper(nums, n1, 0);
+        return n1;
+    }
+
+    private static void permuteHelper(int[] nums, List<List<Integer>> n1, int index) {
+        if (index >= nums.length) {
+            List<Integer> main = new ArrayList<Integer>();
+            for (int i : nums) {
+                main.add(i);
+            }
+            n1.add(main);
+        } else {
+            for (int i = index; i < nums.length; i++) {
+                permuteSwapHelper(nums, index, i);
+                permuteHelper(nums, n1, index + 1);
+                permuteSwapHelper(nums, index, i);
+            }
+        }
+    }
+
+    private static void permuteSwapHelper(int[] nums, int n, int m) {
+        int main = nums[n];
+        nums[n] = nums[m];
+        nums[m] = main;
     }
 }
