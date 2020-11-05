f6e98b159655d1a2185359df6276845d7c59f1e1
zhamid01 <zhamid01@uw.edu>
2020-11-04 19:00:03 -0800

    Problem3 Resource

--- src/main/java/Problem3/Problem3.java
+++ src/main/java/Problem3/Problem3.java
@@ -4,6 +4,11 @@ import Problem2.ListNode;
 
 import java.io.PrintStream;
 
+/*
+This site ws used as help in this code:
+https://www.geeksforgeeks.org/print-reverse-of-a-linked-list-without-actually-reversing/
+*/
+
 public class Problem3 {
     public static void printListInReverse(ListNode list, PrintStream print) {
         if (list == null) {
